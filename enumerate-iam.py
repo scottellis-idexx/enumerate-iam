3d55389f962e7f22ed662d221d68984b7252fb4a
Scott Ellis <scott-ellis@idexx.com>
2019-08-21 09:50:33 -0400

    enumerate-iam: Add verbose switch to show returned results

--- enumerate-iam.py
+++ enumerate-iam.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+
 import argparse
+import pprint
 
 from enumerate_iam.main import enumerate_iam
 
@@ -11,13 +13,23 @@ def main():
     parser.add_argument('--secret-key', help='AWS secret key', required=True)
     parser.add_argument('--session-token', help='STS session token')
     parser.add_argument('--region', help='AWS region to send API requests to', default='us-east-1')
+    parser.add_argument('--verbose', help='Dump output results', action='store_true', default=False)
 
     args = parser.parse_args()
 
-    enumerate_iam(args.access_key,
+    output = enumerate_iam(args.access_key,
                   args.secret_key,
                   args.session_token,
                   args.region)
+
+    if args.verbose:
+        pp = pprint.PrettyPrinter(indent=4)
+
+        print('===== iam =====')
+        pp.pprint(output['iam'])
+
+        print('\n===== bruteforce =====')
+        pp.pprint(output['bruteforce'])
 
 
 if __name__ == '__main__':
